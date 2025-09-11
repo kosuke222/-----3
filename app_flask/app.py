162c574e961621ee2ca197a9efcd346e62a8886c
hiratsuka <j23356ht@edu.tuis.ac.jp>
2025-09-11 18:09:54 +0900

    ハッシュ値の入力画面の 実装

--- app_flask/app.py
+++ app_flask/app.py
@@ -34,7 +34,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
     f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:"
     f"{os.getenv('POSTGRES_PASSWORD')}@"
     f"{os.getenv('POSTGRES_HOST')}:"
-    f"{os.getenv('POSTGRES_PORT')}/"
+    f"{os.getenv('POSTGRES_PORT', '5432')}/"
     f"{os.getenv('POSTGRES_DB')}"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
